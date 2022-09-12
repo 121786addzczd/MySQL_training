8dacf615eba0c1e1bcea583bfb608bbd157bc99b
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-09-12 21:58:28 +0900

    文字列関数の続き

--- string_function.sql
+++ string_function.sql
@@ -22,3 +22,37 @@ SELECT name, CHAR_LENGTH(name) AS name_length FROM employees WHERE CHAR_LENGTH(n
 
 -- UPDATEして空白を削除したものにする
 UPDATE employees SET name = TRIM(name) <> CHAR_LENGTH(TRIM(name));
+
+
+-- REPLACE: 置換
+SELECT REPLACE("I like an apple", "apple", "lemon");  -- 引数２つ目は置き換えたい値、3つ目は置き換える値
+
+SELECT REPLACE(name, "Mrs", "Ms") FROM users WHERE name LIKE 'Mrs%';
+
+-- REPLACEで変換した結果をupdateする
+UPDATE users SET name = REPLACE(name, "Mrs", "Ms") WHERE name LIKE 'Mrs%';
+
+SELECT * FROM users;
+
+
+-- UPPER LOWER(大文字、小文字)
+
+SELECT UPPER("apple");
+SELECT LOWER("APPLE");
+
+-- 全て大文字、全て小文字
+SELECT name, UPPER(name), LOWER(name) FROM users;
+
+
+-- SUBSTRING 一部取り出し
+SELECT SUBSTRING(name, 2,3), name FROM employees;
+
+-- nameカラム二文字目が「田」のあるデータを取り出す
+SELECT * FROM employees WHERE SUBSTRING(name, 2, 1) = "田";
+
+
+-- REVERSE: 逆順
+SELECT REVERSE(name), name FROM employees;
+
+
+
